4f1a2dbe55a9b2dc23cd40f5b4652834a0f7dac4
Lavon Green <lavon_green@yahoo.com>
2022-05-31 03:27:46 -0400

    refactered code and added dependancies

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,9 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      user: 'root',
       dialect: 'mysql',
+      password: 'Lavon240723$$$$',
       dialectOptions: {
         decimalNumbers: true,
       },
